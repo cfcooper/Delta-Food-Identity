7db090b54a95505f74073323ba39f4458bc8f2f4
Courtney Cooper <courtneyfaycooper@gmail.com>
2025-10-03 15:18:33 -0500

    cleaning finished, model updates

--- survey3_cleaning.R
+++ survey3_cleaning.R
@@ -13,54 +13,47 @@ rm(list=ls()) # Caution: this clears the Environment
 dat <- read.csv("rawdata/DWTP_3.csv")
 #premium <- read.csv("cleaneddata/wtpsummary.csv")
 
+column_lookup <- data.frame(    # column lookup
+  column_number = 1:ncol(dat),
+  column_name = names(dat)
+)
+
 dat <- dat[!is.na(dat$full_launch), ]
 names(dat) <- trimws(names(dat))
 dat_b <- dat[dat$black != 0, ]
 dat_d <- dat[dat$black != 1, ]
 
 
-dat_b <- select(dat_b, c(1,35:49,79:98))  # select columns
+
+dat_b <- select(dat_b, c(1,35:49,89:98))  # select columns
 dat_b$melon_price <- 5
 dat_b$corn_price <- .33
 dat_b$tater_price <- 1
 dat_b$mater_price <- 1
 dat_b$rice_price <- 1.75
 
-
-# ****** melon ******
-melon_price_b <- select(dat_b, c(1:4,29:30,37))
-
-melon_price_b <- melon_price_b %>%
-  pivot_longer(
-    cols = c(2:4), # Specify the columns to pivot
-    names_to = "question",   # Name of the new column storing original column names
-    values_to = "choice"      # Name of the new column storing the values
-  )
-melon_price_b <- melon_price_b[!is.na(melon_price_b$choice), ]
-melon_price_b$price <- 5
-melon_price_b$price <- if_else(melon_price_b$question %in% c("melonb_low"), melon_price_b$melon_low, melon_price_b$price)
-melon_price_b$price <- if_else(melon_price_b$question %in% c("melonb_high"), melon_price_b$melon_high, melon_price_b$price)
-melon_price_b <- select(melon_price_b, c(1,4,6:7))
-
-melon_price_b <- melon_price_b %>%
-  rename(p_1 = melon_price)
-melon_price_b <- melon_price_b %>%
-  rename(p_2 = price)
-
-melon_price_b$av_1 <- 1
-melon_price_b$av_2 <- 1
+column_lookup <- data.frame(    # column lookup
+  column_number = 1:ncol(dat_d),
+  column_name = names(dat_d)
+)
 
 
+dat_d <- select(dat_d, c(1,16:30,89:98))  # select columns
+dat_d$melon_price <- 5
+dat_d$corn_price <- .33
+dat_d$tater_price <- 1
+dat_d$mater_price <- 1
+dat_d$rice_price <- 1.75
+
+## MELON ----------------------
 
 #yes/yes
 dat_b$melonyy <- if_else(dat_b$melonb==2 & dat_b$melonb_high==2, 1, 0)
 dat_d$melonyy <- if_else(dat_d$melond==2 & dat_d$melond_high==2, 1, 0)
 
-
 #yes/no
 dat_b$melonyn <- if_else(dat_b$melonb==2 & dat_b$melonb_high==1, 1, 0)
 dat_d$melonyn <- if_else(dat_d$melond==2 & dat_d$melond_high==1, 1, 0)
-
 
 #no/yes
 dat_b$melonny <- if_else(dat_b$melonb==1 & dat_b$melonb_low==2, 1, 0)
@@ -71,54 +64,280 @@ dat_b$melonnn <- if_else(dat_b$melonb==1 & dat_b$melonb_low==1, 1, 0)
 dat_d$melonnn <- if_else(dat_d$melond==1 & dat_d$melond_low==1, 1, 0)
 
 
+#create choice variables
+dat_b$melonc1 <- if_else(dat_b$melonyn==1 | dat_b$melonyy==1, 1, 0)
+dat_b$melonc2 <- if_else(dat_b$melonny==1 | dat_b$melonyy==1, 1, 0)
+
+dat_d$melonc1 <- if_else(dat_d$melonyn==1 | dat_d$melonyy==1, 1, 0)
+dat_d$melonc2 <- if_else(dat_d$melonny==1 | dat_d$melonyy==1, 1, 0)
+
+#create premiums
+dat_b$melonpremvar1 <- 0
+dat_d$melonpremvar1 <- 0
+
+dat_b$melonpremvar2 <- if_else(dat_b$melonyn==1 | dat_b$melonyy==1, 
+                               ((dat_b$melon_high - dat_b$melon_price)/dat_b$melon_price), 0)
+
+dat_d$melonpremvar2 <- if_else(dat_d$melonyn==1 | dat_d$melonyy==1, 
+                               ((dat_d$melon_high - dat_d$melon_price)/dat_d$melon_price), 0)
+dat_b$melonpc1 <- dat_b$melonpremvar1
+dat_b$melonpc2 <- dat_b$melonpremvar2
+dat_d$melonpc1 <- dat_b$melonpremvar1
+dat_d$melonpc2 <- dat_b$melonpremvar2
+
+#set prices
+dat_b$melonp1 <- dat_b$melon_price
+dat_d$melonp1 <- dat_d$melon_price
+
+
+dat_b$melonp2 <- if_else(dat_b$melonyy==1 | dat_b$melonyn==1,
+                         dat_b$melon_high, 0)
+dat_b$melonp2 <- if_else(dat_b$melonnn==1 | dat_b$melonny==1,
+                         dat_b$melon_low, dat_b$melonp2)
+
+dat_d$melonp2 <- if_else(dat_d$melonyy==1 | dat_d$melonyn==1,
+                         dat_d$melon_high, 0)
+dat_d$melonp2 <- if_else(dat_d$melonnn==1 | dat_d$melonny==1,
+                         dat_d$melon_low, dat_d$melonp2)
+
+
+## rice ----------------------
+#yes/yes
+dat_b$riceyy <- if_else(dat_b$riceb==2 & dat_b$riceb_high==2, 1, 0)
+dat_d$riceyy <- if_else(dat_d$riced==2 & dat_d$riced_high==2, 1, 0)
+
+#yes/no
+dat_b$riceyn <- if_else(dat_b$riceb==2 & dat_b$riceb_high==1, 1, 0)
+dat_d$riceyn <- if_else(dat_d$riced==2 & dat_d$riced_high==1, 1, 0)
+
+#no/yes
+dat_b$riceny <- if_else(dat_b$riceb==1 & dat_b$riceb_low==2, 1, 0)
+dat_d$riceny <- if_else(dat_d$riced==1 & dat_d$riced_low==2, 1, 0)
+
+#no/no
+dat_b$ricenn <- if_else(dat_b$riceb==1 & dat_b$riceb_low==1, 1, 0)
+dat_d$ricenn <- if_else(dat_d$riced==1 & dat_d$riced_low==1, 1, 0)
+
+#create choice variables
+dat_b$ricec1 <- if_else(dat_b$riceyn==1 | dat_b$riceyy==1, 1, 0)
+dat_b$ricec2 <- if_else(dat_b$riceny==1 | dat_b$riceyy==1, 1, 0)
+
+dat_d$ricec1 <- if_else(dat_d$riceyn==1 | dat_d$riceyy==1, 1, 0)
+dat_d$ricec2 <- if_else(dat_d$riceny==1 | dat_d$riceyy==1, 1, 0)
+
+
+#create premiums
+dat_b$ricepremvar1 <- 0
+dat_d$ricepremvar1 <- 0
+
+dat_b$ricepremvar2 <- if_else(dat_b$riceyn==1 | dat_b$riceyy==1, 
+                               ((dat_b$rice_high - dat_b$rice_price)/dat_b$rice_price), 0)
+
+dat_d$ricepremvar2 <- if_else(dat_d$riceyn==1 | dat_d$riceyy==1, 
+                               ((dat_d$rice_high - dat_d$rice_price)/dat_d$rice_price), 0)
+dat_b$ricepc1 <- dat_b$ricepremvar1
+dat_b$ricepc2 <- dat_b$ricepremvar2
+dat_d$ricepc1 <- dat_b$ricepremvar1
+dat_d$ricepc2 <- dat_b$ricepremvar2
+
+#set prices
+dat_b$ricep1 <- dat_b$rice_price
+dat_d$ricep1 <- dat_d$rice_price
+
+
+dat_b$ricep2 <- if_else(dat_b$riceyy==1 | dat_b$riceyn==1,
+                         dat_b$rice_high, 0)
+dat_b$ricep2 <- if_else(dat_b$ricenn==1 | dat_b$riceny==1,
+                         dat_b$rice_low, dat_b$ricep2)
+
+dat_d$ricep2 <- if_else(dat_d$riceyy==1 | dat_d$riceyn==1,
+                         dat_d$rice_high, 0)
+dat_d$ricep2 <- if_else(dat_d$ricenn==1 | dat_d$riceny==1,
+                         dat_d$rice_low, dat_d$ricep2)
+
+## tater ----------------------
+#yes/yes
+dat_b$tateryy <- if_else(dat_b$taterb==2 & dat_b$taterb_high==2, 1, 0)
+dat_d$tateryy <- if_else(dat_d$taterd==2 & dat_d$taterd_high==2, 1, 0)
+
+#yes/no
+dat_b$tateryn <- if_else(dat_b$taterb==2 & dat_b$taterb_high==1, 1, 0)
+dat_d$tateryn <- if_else(dat_d$taterd==2 & dat_d$taterd_high==1, 1, 0)
+
+#no/yes
+dat_b$taterny <- if_else(dat_b$taterb==1 & dat_b$taterb_low==2, 1, 0)
+dat_d$taterny <- if_else(dat_d$taterd==1 & dat_d$taterd_low==2, 1, 0)
+
+#no/no
+dat_b$taternn <- if_else(dat_b$taterb==1 & dat_b$taterb_low==1, 1, 0)
+dat_d$taternn <- if_else(dat_d$taterd==1 & dat_d$taterd_low==1, 1, 0)
+
+#create choice variables
+dat_b$taterc1 <- if_else(dat_b$tateryn==1 | dat_b$tateryy==1, 1, 0)
+dat_b$taterc2 <- if_else(dat_b$taterny==1 | dat_b$tateryy==1, 1, 0)
+
+dat_d$taterc1 <- if_else(dat_d$tateryn==1 | dat_d$tateryy==1, 1, 0)
+dat_d$taterc2 <- if_else(dat_d$taterny==1 | dat_d$tateryy==1, 1, 0)
+
+#create premiums
+dat_b$taterpremvar1 <- 0
+dat_d$taterpremvar1 <- 0
+
+dat_b$taterpremvar2 <- if_else(dat_b$tateryn==1 | dat_b$tateryy==1, 
+                               ((dat_b$tater_high - dat_b$tater_price)/dat_b$tater_price), 0)
+
+dat_d$taterpremvar2 <- if_else(dat_d$tateryn==1 | dat_d$tateryy==1, 
+                               ((dat_d$tater_high - dat_d$tater_price)/dat_d$tater_price), 0)
+dat_b$taterpc1 <- dat_b$taterpremvar1
+dat_b$taterpc2 <- dat_b$taterpremvar2
+dat_d$taterpc1 <- dat_b$taterpremvar1
+dat_d$taterpc2 <- dat_b$taterpremvar2
+
+#set prices
+dat_b$taterp1 <- dat_b$tater_price
+dat_d$taterp1 <- dat_d$tater_price
+
+
+dat_b$taterp2 <- if_else(dat_b$tateryy==1 | dat_b$tateryn==1,
+                         dat_b$tater_high, 0)
+dat_b$taterp2 <- if_else(dat_b$taternn==1 | dat_b$taterny==1,
+                         dat_b$tater_low, dat_b$taterp2)
+
+dat_d$taterp2 <- if_else(dat_d$tateryy==1 | dat_d$tateryn==1,
+                         dat_d$tater_high, 0)
+dat_d$taterp2 <- if_else(dat_d$taternn==1 | dat_d$taterny==1,
+                         dat_d$tater_low, dat_d$taterp2)
+
+## corn ----------------------
+#yes/yes
+dat_b$cornyy <- if_else(dat_b$cornb==2 & dat_b$cornb_high==2, 1, 0)
+dat_d$cornyy <- if_else(dat_d$cornd==2 & dat_d$cornd_high==2, 1, 0)
+
+#yes/no
+dat_b$cornyn <- if_else(dat_b$cornb==2 & dat_b$cornb_high==1, 1, 0)
+dat_d$cornyn <- if_else(dat_d$cornd==2 & dat_d$cornd_high==1, 1, 0)
+
+#no/yes
+dat_b$cornny <- if_else(dat_b$cornb==1 & dat_b$cornb_low==2, 1, 0)
+dat_d$cornny <- if_else(dat_d$cornd==1 & dat_d$cornd_low==2, 1, 0)
+
+#no/no
+dat_b$cornnn <- if_else(dat_b$cornb==1 & dat_b$cornb_low==1, 1, 0)
+dat_d$cornnn <- if_else(dat_d$cornd==1 & dat_d$cornd_low==1, 1, 0)
+
+
+#create choice variables
+dat_b$cornc1 <- if_else(dat_b$cornyn==1 | dat_b$cornyy==1, 1, 0)
+dat_b$cornc2 <- if_else(dat_b$cornny==1 | dat_b$cornyy==1, 1, 0)
+
+dat_d$cornc1 <- if_else(dat_d$cornyn==1 | dat_d$cornyy==1, 1, 0)
+dat_d$cornc2 <- if_else(dat_d$cornny==1 | dat_d$cornyy==1, 1, 0)
+
+
+#create premiums
+dat_b$cornpremvar1 <- 0
+dat_d$cornpremvar1 <- 0
+
+dat_b$cornpremvar2 <- if_else(dat_b$cornyn==1 | dat_b$cornyy==1, 
+                               ((dat_b$corn_high - dat_b$corn_price)/dat_b$corn_price), 0)
+
+dat_d$cornpremvar2 <- if_else(dat_d$cornyn==1 | dat_d$cornyy==1, 
+                               ((dat_d$corn_high - dat_d$corn_price)/dat_d$corn_price), 0)
+dat_b$cornpc1 <- dat_b$cornpremvar1
+dat_b$cornpc2 <- dat_b$cornpremvar2
+dat_d$cornpc1 <- dat_b$cornpremvar1
+dat_d$cornpc2 <- dat_b$cornpremvar2
+
+#set prices
+dat_b$cornp1 <- dat_b$corn_price
+dat_d$cornp1 <- dat_d$corn_price
+
+
+dat_b$cornp2 <- if_else(dat_b$cornyy==1 | dat_b$cornyn==1,
+                         dat_b$corn_high, 0)
+dat_b$cornp2 <- if_else(dat_b$cornnn==1 | dat_b$cornny==1,
+                         dat_b$corn_low, dat_b$cornp2)
+
+dat_d$cornp2 <- if_else(dat_d$cornyy==1 | dat_d$cornyn==1,
+                         dat_d$corn_high, 0)
+dat_d$cornp2 <- if_else(dat_d$cornnn==1 | dat_d$cornny==1,
+                         dat_d$corn_low, dat_d$cornp2)
+
+## mater ----------------------
+#yes/yes
+dat_b$materyy <- if_else(dat_b$materb==2 & dat_b$materb_high==2, 1, 0)
+dat_d$materyy <- if_else(dat_d$materd==2 & dat_d$materd_high==2, 1, 0)
+
+#yes/no
+dat_b$materyn <- if_else(dat_b$materb==2 & dat_b$materb_high==1, 1, 0)
+dat_d$materyn <- if_else(dat_d$materd==2 & dat_d$materd_high==1, 1, 0)
+
+#no/yes
+dat_b$materny <- if_else(dat_b$materb==1 & dat_b$materb_low==2, 1, 0)
+dat_d$materny <- if_else(dat_d$materd==1 & dat_d$materd_low==2, 1, 0)
+
+#no/no
+dat_b$maternn <- if_else(dat_b$materb==1 & dat_b$materb_low==1, 1, 0)
+dat_d$maternn <- if_else(dat_d$materd==1 & dat_d$materd_low==1, 1, 0)
+
+
+#create choice variables
+dat_b$materc1 <- if_else(dat_b$materyn==1 | dat_b$materyy==1, 1, 0)
+dat_b$materc2 <- if_else(dat_b$materny==1 | dat_b$materyy==1, 1, 0)
+
+dat_d$materc1 <- if_else(dat_d$materyn==1 | dat_d$materyy==1, 1, 0)
+dat_d$materc2 <- if_else(dat_d$materny==1 | dat_d$materyy==1, 1, 0)
+
+
+#create premiums
+dat_b$materpremvar1 <- 0
+dat_d$materpremvar1 <- 0
+
+dat_b$materpremvar2 <- if_else(dat_b$materyn==1 | dat_b$materyy==1, 
+                               ((dat_b$mater_high - dat_b$mater_price)/dat_b$mater_price), 0)
+
+dat_d$materpremvar2 <- if_else(dat_d$materyn==1 | dat_d$materyy==1, 
+                               ((dat_d$mater_high - dat_d$mater_price)/dat_d$mater_price), 0)
+dat_b$materpc1 <- dat_b$materpremvar1
+dat_b$materpc2 <- dat_b$materpremvar2
+dat_d$materpc1 <- dat_b$materpremvar1
+dat_d$materpc2 <- dat_b$materpremvar2
+
+#set prices
+dat_b$materp1 <- dat_b$mater_price
+dat_d$materp1 <- dat_d$mater_price
+
+
+dat_b$materp2 <- if_else(dat_b$materyy==1 | dat_b$materyn==1,
+                         dat_b$mater_high, 0)
+dat_b$materp2 <- if_else(dat_b$maternn==1 | dat_b$materny==1,
+                         dat_b$mater_low, dat_b$materp2)
+
+dat_d$materp2 <- if_else(dat_d$materyy==1 | dat_d$materyn==1,
+                         dat_d$mater_high, 0)
+dat_d$materp2 <- if_else(dat_d$maternn==1 | dat_d$materny==1,
+                         dat_d$mater_low, dat_d$materp2)
+
+# save all dataframes for running models ----------------
 
 
 
-# rice
-rice_price_b <- select(dat_b, c(1,11:13,33:34,41))
+dat_d$info_treatment <- "delta"
+dat_b$info_treatment <- "black"
 
-rice_price_b <- rice_price_b %>%
-  pivot_longer(
-    cols = c(2:4), # Specify the columns to pivot
-    names_to = "question",   # Name of the new column storing original column names
-    values_to = "choice"      # Name of the new column storing the values
-  )
-rice_price_b <- rice_price_b[!is.na(rice_price_b$choice), ]
-rice_price_b$price <- 1.75
-rice_price_b$price <- if_else(rice_price_b$question %in% c("rice_low"), rice_price_b$rice_low.1, rice_price_b$price)
-rice_price_b$price <- if_else(rice_price_b$question %in% c("rice_high"), rice_price_b$rice_high.1, rice_price_b$price)
-rice_price_b <- select(rice_price_b, c(1,4,6:7))
 
-rice_price_b <- rice_price_b %>%
-  rename(p_1 = rice_price)
-rice_price_b <- rice_price_b %>%
-  rename(p_2 = price)
+column_lookup <- data.frame(    # column lookup
+  column_number = 1:ncol(dat_d),
+  column_name = names(dat_d)
+)
+
+dat_d <- dat_d[c(1,17:92)]
+dat_b <- dat_b[c(1,17:92)]
+
+full_dat <- rbind(dat_d,dat_b)
+saveRDS(dat_d, "cleaneddata/df_delta.rds")
+saveRDS(dat_b, "cleaneddata/df_black.rds")
+saveRDS(full_dat, "cleaneddata/df_full.rds")
 
 
 
-
-
-# summary info
-wtpdat <- premium[complete.cases(premium), ]
-
-wtpdat$calcwtp <- (wtpdat$price*wtpdat$value)+wtpdat$price
-wtpdat_all <- wtpdat[wtpdat$infotreatment == "all", ]
-wtpdat_rice <- wtpdat[wtpdat$crop == "rice", ]                                                                                                                                                                                                     .
-wtpdat_watermelon <- wtpdat[wtpdat$crop == "watermelon", ]
-
-# tables ggplot
-
-r <- ggplot() + geom_col(data= wtpdat_rice, 
-                    aes(x = infotreatment, y= calcwtp, fill = region), 
-                    position = "dodge") + theme_minimal()
-
-r + geom_hline(yintercept=1.75, linetype="dashed", color = "black")
-
-
-
-
-w <- ggplot() + geom_col(data= wtpdat_watermelon, 
-                         aes(x = infotreatment, y= calcwtp, fill = region), 
-                         position = "dodge") + theme_minimal()
-
-w + geom_hline(yintercept=1.75, linetype="dashed", color = "black")
--- survey3_wtp.R
+++ survey3_wtp.R
@@ -2,19 +2,11 @@
 
 
 
-
 # Install Icens from Bioconductor (required for DCchoice)
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install("Icens")
 
-# Install DCchoice if needed
-if (!require("DCchoice", quietly = TRUE))
-  install.packages("DCchoice")
-
-
-
-# Load required packages
 library(DCchoice)
 library(dplyr)
 
@@ -22,13 +14,100 @@ library(dplyr)
 # Clear workspace
 rm(list = ls())
 
-# Import data
-delta <- read.csv("rawdata/DWTP_3.csv", stringsAsFactors = FALSE)
 
-# Trim whitespace from column names
-names(delta) <- trimws(names(delta))
+# read in dataframes
 
-# Trim whitespace from all character columns
-delta <- delta %>%
-  mutate(across(where(is.character), trimws))
+df_bl <- readRDS("cleaneddata/df_black.rds")
+df_de <- readRDS("cleaneddata/df_delta.rds")
+df <- readRDS("cleaneddata/df_full.rds")
+
+# run models ------------
+
+library(mlogit)
+
+
+# rice
+library(mlogit)
+
+# Step 1: Create long format for DCE
+df_long <- data.frame(
+  chid = rep(1:nrow(df), each = 2),
+  alternative = rep(c("non_delta", "delta"), nrow(df)),
+  chosen = c(rbind(1 - df$ricec1, df$ricec1)),
+  price = c(rbind(df$ricep1, df$ricep2))
+)
+
+# Add treatment variable (constant across alternatives for each choice)
+df_long$info_treatment <- rep(df$info_treatment, each = 2)
+
+# Step 2: Prepare data for mlogit
+df_mlogit <- mlogit.data(df_long, 
+                         choice = "chosen",
+                         shape = "long",
+                         alt.var = "alternative",
+                         chid.var = "chid")
+
+# Step 3: Basic conditional logit model
+# Price varies across alternatives, delta dummy in alternative-specific part
+model_basic <- mlogit(chosen ~ price | 0, data = df_mlogit)
+summary(model_basic)
+
+# Step 4: Model with delta-specific constant (MAIN MODEL)
+model_delta <- mlogit(chosen ~ price | alternative, data = df_mlogit)
+summary(model_delta)
+
+# Calculate WTP premium for delta
+delta_coef <- coef(model_delta)["alternativedelta"]
+price_coef <- coef(model_delta)["price"]
+
+wtp_premium <- -delta_coef / price_coef
+print(paste("WTP premium for delta: $", round(wtp_premium, 2)))
+
+# Step 5: Model with treatment effects
+# Need to create treatment interaction manually in long format
+df_long$delta_treat <- ifelse(df_long$alternative == "delta", 
+                              df_long$info_treatment, 0)
+
+# Re-prepare mlogit data
+df_mlogit2 <- mlogit.data(df_long, 
+                          choice = "chosen",
+                          shape = "long",
+                          alt.var = "alternative",
+                          chid.var = "chid")
+
+# Model with treatment interaction
+model_treatment <- mlogit(chosen ~ price + delta_treat | alternative, 
+                          data = df_mlogit2)
+summary(model_treatment)
+
+# Calculate WTP by treatment group
+delta_coef <- coef(model_treatment)["alternativedelta"]
+treatment_coef <- coef(model_treatment)["delta_treat"]
+price_coef <- coef(model_treatment)["price"]
+
+wtp_control <- -delta_coef / price_coef
+wtp_info <- -(delta_coef + treatment_coef) / price_coef
+
+print(paste("WTP premium - Control: $", round(wtp_control, 2)))
+print(paste("WTP premium - Info: $", round(wtp_info, 2)))
+print(paste("Treatment effect: $", round(wtp_info - wtp_control, 2)))
+
+# Step 6: Get standard errors for WTP using delta method
+library(car)
+
+# For basic model
+wtp_se_basic <- deltaMethod(model_delta, "-alternativedelta/price", 
+                            parameterNames = names(coef(model_delta)))
+print(wtp_se_basic)
+
+# For treatment model - control group
+wtp_se_control <- deltaMethod(model_treatment, "-alternativedelta/price",
+                              parameterNames = names(coef(model_treatment)))
+print(wtp_se_control)
+
+# For treatment model - info group
+wtp_se_info <- deltaMethod(model_treatment, 
+                           "-(alternativedelta + delta_treat)/price",
+                           parameterNames = names(coef(model_treatment)))
+print(wtp_se_info)
 
